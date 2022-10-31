3f0563cca5cf20f29f129c0d16bbaae65f18d24c
ejimaone <abdulwahabonozutu@gmail.com>
2022-10-31 10:52:11 +0100

    added profilepic and share button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Cards from "./components/Cards";
+import "./App.css";
+import img from "./IMG_20221028_203259.jpg";
+import sharebtn from "./sharebtn.jpg";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <img src={sharebtn} alt="share" className="sharebtn"></img>
+      <img src={img} className="profile_pic" alt="Abdulwahab's "></img>
+      <div className="name">Abdulwahab Yusuf</div>
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
